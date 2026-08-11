091972be99df99898819afcdda990a8f877a1980
Javiera Mosqueira <jmosqueira@miuandes.cl>
2026-08-11 13:35:58 -0400

    Agrega pruebas para matrices rectangulares

--- tests/test_mimatmul.py
+++ tests/test_mimatmul.py
@@ -3,36 +3,38 @@ import pytest
 
 from src.mimatmul import mimatmul
 
-
+#Prueba 1
 def test_caso_pequeno_conocido():
     A = [[1, 2], [3, 4]]
     B = [[5, 6], [7, 8]]
     resultado = mimatmul(A, B)
     assert np.allclose(resultado, [[19, 22], [43, 50]])
 
-
+#Prueba 2
 def test_matrices_cuadradas():
     A = [[2, 0, 1], [3, 0, 0], [5, 1, 1]]
     B = [[1, 0, 1], [1, 2, 1], [1, 1, 0]]
     resultado = mimatmul(A, B)
     assert np.allclose(resultado, [[3, 1, 2], [3, 0, 3], [7, 3, 6]])
 
-
+#Prueba 3
 def test_matrices_rectangulares():
     A = [[1, 2, 3], [4, 5, 6]]
     B = [[7, 8], [9, 10], [11, 12]]
     resultado = mimatmul(A, B)
     assert np.allclose(resultado, [[58, 64], [139, 154]])
 
-
+#Prueba 4
 def test_comparacion_con_numpy():
     A = np.array([[1, 0, 2, -1], [3, 1, 0, 2], [-2, 1, 1, 0]])
     B = np.array([[1, 2], [0, 1], [3, -1], [1, 0]])
     assert np.allclose(mimatmul(A, B), A @ B)
 
-
+#Prueba 5
 def test_dimensiones_incompatibles():
     A = [[1, 2], [3, 4]]
     B = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     with pytest.raises(ValueError, match="incompatibles"):
         mimatmul(A, B)
+
+#Comprobar que el test funciona correctamente: python -m pytest -v
